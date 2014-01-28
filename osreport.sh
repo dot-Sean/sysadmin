ba8cddfa3a77a8dbdf7c562ca11163921d5504ee
tnx <tnx@polifemo.nellinux.net>
2014-01-28 23:50:28 +0100

    * osreport.sh: fix missing duration and font.

--- osreport.sh
+++ osreport.sh
@@ -7,6 +7,10 @@
 # 2014 Jan 28 - nellinux@rocketmail.com
 
 
+duration=3
+
+font=-*-terminus-medium-*-*-*-*-140-*-*-*-*-*-*
+
 pkill -9 -U `id -u` osd_cat  2>&1 > /dev/null
 
 date | osd_cat -d $duration -l5 -c yellow -f $font &
