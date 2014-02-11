0f4607645aae228e3c02bf325a52928699b8846d
tnx <tnx@polifemo.nellinux.net>
2014-02-11 22:58:16 +0100

    * osreport.sh: prints desktop name also. (get_ws_name): new function (from http://darkshed.net/files/texts/fluxbox-show-wsname-via-osd.txt).

--- osreport.sh
+++ osreport.sh
@@ -13,6 +13,43 @@ duration=10
 
 font=-*-terminus-medium-*-*-*-*-180-*-*-*-*-*-*
 
+
+
+#
+# Dependencies
+#
+
+for d in osd_cat xprop pkill; do
+    type $d > /dev/null || exit 1
+done
+
+
+#
+# Functions
+#
+get_ws_name() {
+wsname=$(xprop -root _NET_CURRENT_DESKTOP \
+    _NET_NUMBER_OF_DESKTOPS \
+    _NET_DESKTOP_NAMES | \
+    awk '
+        /_NET_CURRENT_DESKTOP/ { current = $3 + 1; }
+        /_NET_NUMBER_OF_DESKTOPS/ { no_ws = $3; }
+        /_NET_DESKTOP_NAMES/ { for (i = 3; i < no_ws + 3; i++) {
+                                 names[i - 3] = $i;
+                                 gsub( "\"|,", " ", names[i - 3]);
+                                 gsub ("[[:space:]]*", "", names[i - 3]);
+                               };
+                             };
+        END {
+          print names[current - 1]" "current"/"no_ws;
+        };')
+
+}
+
+
 pkill -9 -U `id -u` osd_cat  2>&1 > /dev/null
 
-date | osd_cat --delay $duration --lines 2 -c yellow --font $font --pos=bottom &
+get_ws_name
+d=`date | sed 's/CET.*//'`
+
+echo -e "$d\n$wsname"  | osd_cat --delay $duration --lines 2 -c yellow --font $font --pos=bottom &
